6bc83c291d25c387b441e5ffaa33436f0ed619e1
andrew_kittredge <andrewlkittredge@gmail.com>
2011-07-04 13:17:17 -0400

    Working.

--- next_train.py
+++ next_train.py
@@ -13,20 +13,20 @@ from lxml import etree
 
 BACK_BAY_TAG = 'place-bbsta'
 
-PREDICTION_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=mbta&d=%(direction_tag)s&s=%(stop_tag)s'
+PREDICTION_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=mbta&s=%(stop_tag)s&r=%(route_tag)s'
 
-PREDICTION_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=mbta&r=39&s=22365&useShortTitles=true'
+#PREDICTION_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=mbta&r=39&s=22365&useShortTitles=true'
 
 
 def get_params():
     try:
-        route, direction =sys.argv[1:3]
+        route, direction, stop =sys.argv[1:4]
     except ValueError:
         #try something else,
-        route, direction = (None,) * 2
+        route, direction, stop = (None,) * 3
         
 
-    return route, direction
+    return route, direction, stop
 
 def title_and_times_xpath(stream):
     tree = etree.parse(stream)
@@ -46,19 +46,19 @@ def print_title_and_times(route, stop, predicted_arrival_minutes):
 
 def main():
 
-    route, direction = get_params()
+    route, direction, stop = get_params()
 
     tags = {'route_tag' : route,
                 'direction_tag' : direction,
-                'stop_tag' : BACK_BAY_TAG
+                'stop_tag' : stop
             }
 
     prediction_url = PREDICTION_URL % tags
+    print prediction_url
     prediction_stream = urllib.urlopen(prediction_url)
     prediction_attributes = title_and_times_xpath(prediction_stream)
 
     print_title_and_times(*prediction_attributes)
     
-
 if __name__ == '__main__':
     sys.exit(main())
